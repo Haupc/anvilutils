1dc9e1ac6319d06ccf7fc1d102253298412babc8
haupc <haupc.uet@gmail.com>
2024-02-16 00:01:23 +0700

    feat: set erc20 approval

--- helper/erc20.go
+++ helper/erc20.go
@@ -1,14 +1,24 @@
 package helper
 
 import (
+	"math/big"
+
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/haupc/foundryutils/contracts"
 )
 
 // pack data for balanceOf method
 // solidity: balanceOf(address)
-func BalanceOfCallData(userAddress common.Address) []byte {
+func Erc20BalanceOfCallData(userAddress common.Address) []byte {
 	abi, _ := contracts.Erc20MetaData.GetAbi()
 	packedData, _ := abi.Pack("balanceOf", userAddress)
+	return packedData
+}
+
+// pack data for approve method
+// solidity: approve(address spender, uint256 amount) returns(bool)
+func Erc20ApproveCallData(spender common.Address, amount *big.Int) []byte {
+	abi, _ := contracts.Erc20MetaData.GetAbi()
+	packedData, _ := abi.Pack("approve", spender, amount)
 	return packedData
 }
